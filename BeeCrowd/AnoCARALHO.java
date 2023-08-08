4ab8649dabbb787ae79f1cfef8ebb5a528216237
Samuel Oliveira Lopes <samu5020ol@gmail.com>
2023-08-08 15:29:46 -0300

    sfd

--- BeeCrowd/AnoCARALHO.java
+++ BeeCrowd/AnoCARALHO.java
@@ -7,26 +7,17 @@ public class AnoCARALHO {
         Scanner x = new Scanner(System.in);
             int a = x.nextInt();
 
+        
+
         int dia = 1;
         int mes = 0;
-        int ano = 0;
+        
 
-            for(int i=0;i<a;i++){
-                if(dia == 30){
-                    mes++;
-                    dia = 0;
-                }
-
-                if(mes == 12){
-                    ano++;
-                    mes = 0;
-                }
-
-                dia++;
-            }
-
-            System.out.println(dia);
-            System.out.println(mes);
+           
             System.out.println(ano);
+            System.out.println(mes);
+            System.out.println(dia);
+           
+            
     }
 }
