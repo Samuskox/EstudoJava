dbc2179958b0925a8393d9437e4016c02ab036cb
Samuel Oliveira Lopes <samu5020ol@gmail.com>
2023-08-16 01:10:11 -0300

    efg

--- BeeCrowd/Salario2.java
+++ BeeCrowd/Salario2.java
@@ -8,11 +8,40 @@ public class Salario2 {
 
         Scanner x = new Scanner(System.in);
 
-        String nome = x.nextLine();
+       float s = x.nextFloat();
+       float sp = 0; // salario percentual
+       float ns = 0; // novo salario
 
-        double fixo = x.nextDouble();
-        double feito = x.nextDouble();
-        double total = (feito*0.15) + fixo;
-        System.out.printf("TOTAL = U$ %.2f\n", total);
+       if(s >= 0 && s <= 400 ){
+        sp = (float) (s * 0.15);
+        ns = sp+s;
+        System.out.printf("Novo salario: %.2f\n", ns);
+        System.out.printf("Reajuste ganho: %.2f\n", sp);
+        System.out.println("Em percentual: 15%");
+       } else if(s > 400 && s <= 800 ){
+        sp = (float) (s * 0.12);
+        ns = sp+s;
+        System.out.printf("Novo salario: %.2f\n", ns);
+        System.out.printf("Reajuste ganho: %.2f\n", sp);
+        System.out.println("Em percentual: 12%");
+       } else if(s > 800 && s <= 1200 ){
+        sp = (float) (s * 0.10);
+        ns = sp+s;
+        System.out.printf("Novo salario: %.2f\n", ns);
+        System.out.printf("Reajuste ganho: %.2f\n", sp);
+        System.out.println("Em percentual: 10%");
+       } else if(s > 1200 && s <= 2000 ){
+        sp = (float) (s * 0.07);
+        ns = sp+s;
+        System.out.printf("Novo salario: %.2f\n", ns);
+        System.out.printf("Reajuste ganho: %.2f\n", sp);
+        System.out.println("Em percentual: 7%");
+       } else if(s > 2000 ){
+        sp = (float) (s * 0.04);
+        ns = sp+s;
+        System.out.printf("Novo salario: %.2f\n", ns);
+        System.out.printf("Reajuste ganho: %.2f\n", sp);
+        System.out.println("Em percentual: 4%");
+       }
     }
 }
